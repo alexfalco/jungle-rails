c6eeeb25a4c966d58e0a529ef2638cf82eb2c635
alexfalco <afalconer02@gmail.com>
2020-02-05 21:30:08 -0500

    feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   
   def show
+    @number_products = Product.count
+    @number_categories = Category.count
   end
 end
+
+
